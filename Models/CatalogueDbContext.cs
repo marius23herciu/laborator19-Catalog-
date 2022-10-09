9f8e4ee168201f06505ab47b311f45e6b54c5ab9
marius.herciu23 <marius.herciu23@gmail.com>
2022-10-09 14:17:50 +0300

    ...

--- Models/CatalogueDbContext.cs
+++ Models/CatalogueDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,15 +10,22 @@ namespace laborator19_Catalog_.Models
 {
     public class CatalogueDbContext: DbContext
     {
+        private readonly string connectionString;
         public DbSet<Student> Students { get; set; }
         public DbSet<Address> Adresses { get; set; }
         public DbSet<Mark> Marks { get; set; }
         public DbSet<Subject> Subjects  { get; set; }
         public DbSet<Teacher> Teachers  { get; set; }
 
+
+        public CatalogueDbContext(IConfiguration configuration)
+        {
+            this.connectionString = configuration.GetConnectionString("LocalDb");
+        }
+
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-NCBKPTR\SQLEXPRESS;Initial Catalog=Lab19(Catalogue);Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
+            optionsBuilder.UseSqlServer(this.connectionString);
         }
 
     }
